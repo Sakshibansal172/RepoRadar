f82327c5474e872d6670284138acab147e3cab69
sakshibansal <sakshi.bansal@kongsbergdigital.com>
2025-04-26 14:19:25 +0530

    added tailwind config snd neccessary comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { RepoDetailsComponent } from './components/repo-details/repo-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoComponent,
+    RepoDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/repo-details/repo-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-details',
+  templateUrl: './repo-details.component.html',
+  styleUrl: './repo-details.component.css'
+})
+export class RepoDetailsComponent {
+
+}
