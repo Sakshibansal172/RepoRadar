7fb9a5ff6e54069b96490c400e1203f39f1fdd1e
sakshibansal <sakshi.bansal@kongsbergdigital.com>
2025-04-27 00:51:04 +0530

    fixed ui and added on click details of repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepoComponent } from './components/repo/repo.component';
+import { RepoDetailsComponent } from './components/repo-details/repo-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path:'dashboard', component:RepoComponent},
+  {path: 'project/:owner/:repo',component:RepoDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/repo-details/repo-details.component.ts
+++ src/app/components/repo-details/repo-details.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../../services/github.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-repo-details',
   templateUrl: './repo-details.component.html',
   styleUrl: './repo-details.component.css'
 })
-export class RepoDetailsComponent {
+export class RepoDetailsComponent implements OnInit{
+  repoDetails: any;
+  loading: boolean = true;
+  error: string | null = null;
 
+  constructor(
+    private githubService:GithubService,
+    private route:ActivatedRoute,
+    private location:Location
+  ){ }
+
+  ngOnInit(): void {
+    const owner = this.route.snapshot.paramMap.get('owner');
+    const repo = this.route.snapshot.paramMap.get('repo');
+    if (owner && repo) {
+      this.githubService.getRepoDetails(owner,repo).subscribe({
+        next:(data) => {
+          this.repoDetails = data;
+          this.loading = false;
+        },
+        error:(err) =>{
+          this.error = 'Failed to load repository details.';
+          this.loading = false;
+        }
+      })
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
