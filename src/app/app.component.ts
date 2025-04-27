de583770ee8eca09a513a962c49e94454b35366d
sakshibansal <sakshi.bansal@kongsbergdigital.com>
2025-04-27 11:47:00 +0530

    some ui fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RepoRadar';
+  isSearchStarted: boolean = false;
+  
+  startSearch() {
+    this.isSearchStarted = !this.isSearchStarted;
+  }
 }
